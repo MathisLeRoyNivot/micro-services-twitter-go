21136458e3bafe8f79072af755f9799a73654474
mathislrn <mathis.lrn061198@gmail.com>
2020-02-02 18:51:19 +0100

    feat(app): updating DAO

--- src/service/dao/rating_dao.go
+++ src/service/dao/rating_dao.go
@@ -1,7 +1,7 @@
 package dao
 
 import (
-	. "github.com/mlabouardy/movies-restapi/models"
+	. "../models/rating.go"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"log"
@@ -10,4 +10,45 @@ import (
 type RatingsDAO struct {
 	Server   string
 	Database string
+}
+
+var db *mgo.Database
+
+const (
+	COLLECTION = "ratings"
+)
+
+// Establish a connection to database
+func (r *RatingsDAO) Connect() {
+	session, err := mgo.Dial(r.Server)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = session.DB(r.Database)
+}
+
+// Get all ratings
+func (m *RatingsDAO) FindAll() ([]Rating, error) {
+	var ratings []Rating
+	err := db.C(COLLECTION).Find(bson.M{}).All(&ratings)
+	return ratings, err
+}
+
+// Find rating by id
+func (m *RatingsDAO) FindById(id string) (Rating, error) {
+	var rating Rating
+	err := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&rating)
+	return rating, err
+}
+
+// Insert rating
+func (r *RatingsDAO) Insert(movie Rating) error {
+	err := db.C(COLLECTION).Insert(&movie)
+	return err
+}
+
+// Delete rating
+func (r *RatingsDAO) Delete(rating Rating) error {
+	err := db.C(COLLECTION).Remove(&rating)
+	return err
 }
